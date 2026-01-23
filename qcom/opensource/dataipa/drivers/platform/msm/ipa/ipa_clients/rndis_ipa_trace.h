b7c7215172144b4109ef2be6eeeeec7a1c091829
AnierinB <anierin@evolution-x.org>
2026-01-23 03:41:49 -0800

    treewide: Point to ayn qcs8550
    Change-Id: I42296ae9016dad9445e4d8718437daf5b912e9f8
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_clients/rndis_ipa_trace.h
+++ qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_clients/rndis_ipa_trace.h
@@ -72,7 +72,7 @@ TRACE_EVENT(
 /* This part must be outside protection */
 #undef TRACE_INCLUDE_PATH
 #ifdef CONFIG_IPA_VENDOR_DLKM
-#define TRACE_INCLUDE_PATH ../../../../sm8550-modules/qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_clients
+#define TRACE_INCLUDE_PATH ../../../../qcs8550-modules/qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_clients
 #elif defined(CONFIG_KALAMA_IPA_LE)
 #define TRACE_INCLUDE_PATH ../ipa/ipa_clients
 #elif defined(CONFIG_SCUBA_IPA_LE)
