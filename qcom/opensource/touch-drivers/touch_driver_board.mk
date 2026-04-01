54bb698329c7009be41f0c1d6d89e4c1d858fb41
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2026-04-01 09:23:22 +0530

    touch: Build `synaptics_dsx` used in Odin 2
     * Uses reconstructed header and updated the files to point to the same.
    
    Change-Id: I38bb46fbcd9b093aa46d53761467d9c597630e4f

--- qcom/opensource/touch-drivers/touch_driver_board.mk
+++ qcom/opensource/touch-drivers/touch_driver_board.mk
@@ -26,6 +26,7 @@ ifeq ($(TOUCH_DLKM_ENABLE),  true)
                                         $(KERNEL_MODULES_OUT)/goodix_ts.ko \
                                         $(KERNEL_MODULES_OUT)/atmel_mxt_ts.ko \
                                         $(KERNEL_MODULES_OUT)/qts.ko
+                                        $(KERNEL_MODULES_OUT)/synaptics_dsx.ko
                         else ifeq ($(TARGET_BOARD_PLATFORM), blair)
                                 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/focaltech_fts.ko \
                                         $(KERNEL_MODULES_OUT)/nt36xxx-i2c.ko \
