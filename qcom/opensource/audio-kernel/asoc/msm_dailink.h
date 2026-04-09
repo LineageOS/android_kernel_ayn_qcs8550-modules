6fc6e839c6891c6e5d9d39a87a1e3302e979dce0
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2026-04-09 14:20:12 +0530

    ASoC: aw883xx: Update dai link names to channel-based format
    Update sep_mi2s_rx dai link to use channel-based component and DAI names
    matching the aw883xx driver's rename-flag mode.
    
    Change-Id: I69d7ecdfef21360f068ded7f7ea448e99beaa256

--- qcom/opensource/audio-kernel/asoc/msm_dailink.h
+++ qcom/opensource/audio-kernel/asoc/msm_dailink.h
@@ -282,8 +282,8 @@ SND_SOC_DAILINK_DEFS(sen_mi2s_tx,
 
 SND_SOC_DAILINK_DEFS(sep_mi2s_rx,
 	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
-	DAILINK_COMP_ARRAY(COMP_CODEC("msm-stub-codec.1", "msm-stub-rx"),
-			   COMP_CODEC("TAS5805m.0-002d", "TAS5805m.0-002d")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("aw883xx_smartpa_0", "aw883xx-aif-0"),
+			   COMP_CODEC("aw883xx_smartpa_1", "aw883xx-aif-1")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
 
 SND_SOC_DAILINK_DEFS(sep_mi2s_tx,
