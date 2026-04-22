715158f72385bc32d5b946acedb4fa625003f77c
Balázs Triszka <info@balika011.hu>
2026-04-22 19:25:58 +0200

    audio: Add aw883xx codec
    Change-Id: I2d58e88b20087cdcea4bbb363e8131468a7d87a3

--- qcom/opensource/audio-kernel/Android.mk
+++ qcom/opensource/audio-kernel/Android.mk
@@ -323,6 +323,16 @@ LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
 
+########################### AW883XX CODEC  ################################
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES           := $(AUDIO_SRC_FILES)
+LOCAL_MODULE              := aw883xx_dlkm.ko
+LOCAL_MODULE_KBUILD_NAME  := asoc/codecs/aw883xx/aw883xx_dlkm.ko
+LOCAL_MODULE_TAGS         := optional
+LOCAL_MODULE_DEBUG_ENABLE := true
+LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/Build_external_kernelmodule.mk
+
 ########################### WCD938x CODEC  ################################
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES           := $(AUDIO_SRC_FILES)
